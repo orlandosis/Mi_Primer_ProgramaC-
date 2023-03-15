cf2b336a813e9773e95260edd00adf6ea69e0c66
sis <sis@DESKTOP-J6FTPD5>
2023-03-15 15:50:46 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,386 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hola Mundo!");
+
+
+/*using System;
+/*namespace program
+{
+    class holamundo
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("hola");
+        }
+    }
+}
+
+*/
+
+
+using System;
+using System.Runtime.CompilerServices;
+
+class MiClaseBonita
+{
+    static void Main()
+    {
+        //string postre = "Gelatina de Fresa22##"; // String
+        //int stock = 46; // Integer 
+        //char calificación =  '1'; // Char 
+        //bool existe = true; // Booleano 
+        //float precio = 3.55123456789F; // Float 
+        //double precios = 29.59123456789D; // Double 
+        //long numero = 73000000000L; // Long 
+
+        //System.Console.WriteLine(postre);
+        //System.Console.WriteLine(stock);
+        //System.Console.WriteLine(calificación);
+        //System.Console.WriteLine(existe);
+        //System.Console.WriteLine(precio);
+        //System.Console.WriteLine(precios);
+        //System.Console.WriteLine(numero);
+
+
+        //int a=0, b=0;
+
+
+        //System.Console.WriteLine(a);
+        //System.Console.WriteLine(b);
+
+        //int suma, multiplicacion, operacion1, operacion2;
+        //int n = 3;
+        //double d, division;
+
+        //suma = n + 3;
+        //multiplicacion= n * 4;
+        //operacion1 = 7 + 2 * 4;
+        //operacion2 = n * (n + 2) * 4;
+        //division= 3.5 / 2;
+        //d = 7 / 4;
+
+        //System.Console.WriteLine(suma);
+        //System.Console.WriteLine(multiplicacion);
+        //System.Console.WriteLine(operacion1);
+        //System.Console.WriteLine(operacion2);
+        //System.Console.WriteLine(division);
+        //System.Console.WriteLine(d);
+
+
+        //int area;
+
+
+        //int longitud = 20;
+        //int ancho = 10;
+
+        //area = longitud * ancho;
+
+        //System.Console.WriteLine(area);
+
+
+
+        //int a, b, c, d;
+
+        //d = -8;
+
+        //a = (1 * 2) + 3;
+        //b = 1 + (2 * 3);
+        //c = (1 + 2) * 3;
+        //c = a + b;
+        //d = -d;
+
+        //System.Console.WriteLine(a);
+        //System.Console.WriteLine(b);
+        //System.Console.WriteLine(c);
+        //System.Console.WriteLine(d);
+
+
+        //double mitad = 1D/ 2D;
+        //double resultado = 20000D * mitad ;
+
+        //System.Console.WriteLine(resultado);
+
+        // int i;
+
+        //// i = 12 % 4;
+        //// i = 13 % 4;
+        ///
+
+        //string web = "forCode";
+        //int b = 1;
+        //string c = web + "" + b;
+
+        //var num = "hola";
+        //var num2 = 5;
+        //var resultado = num + num2;
+        //Console.WriteLine(resultado);
+        //System.Console.WriteLine(b);
+
+        //const int ruedas = 4;
+        //const int espejos = 2;
+
+        //int coches = 5;
+
+        //int totalRuedas, totalEspejos;
+
+        //totalRuedas = ruedas * coches;
+        //totalEspejos = espejos * coches;
+
+        //Console.WriteLine(totalRuedas);
+        //Console.WriteLine(totalEspejos);
+
+        //string apellidoPaterno, nombreCompleto;
+        //string saludo;
+        //string primerNombre = "Mike";
+
+        //apellidoPaterno = "Parr";
+
+        //nombreCompleto = primerNombre +"   "+ apellidoPaterno;
+        //saludo = "saludos de" +"   "+ nombreCompleto;
+
+        //Console.WriteLine("Mi nombre completo es:"+"   " + nombreCompleto);
+        //Console.WriteLine(saludo +" Mucho Gusto");
+
+        //string s1, s2;
+        //int num = 44;
+        //double d = 1.234;
+
+        //s1 = "44";
+        //s2 = Convert.ToString(d);
+
+        //Console.WriteLine(s1);
+        //Console.WriteLine(s2);
+
+
+        //int primerNumero, segundoNumero, suma;
+
+        //Console.Write("Introduce el primer número: ");
+        //primerNumero = Convert.ToInt32(System.Console.ReadLine());
+        //Console.Write("Introduce el segundo número: ");
+        //segundoNumero = Convert.ToInt32(System.Console.ReadLine());
+        //suma = primerNumero + segundoNumero;
+
+        //Console.WriteLine("La suma es= " + suma);
+
+        ////string str = "10";
+
+        //int x = Convert.ToInt32(str);
+
+        //System.Console.WriteLine(x);
+
+        //string cadena1 = "45.25";
+        //string cadena2 = "56";
+
+        //double n_objeto;
+        //n_objeto = Convert.ToDouble(cadena1);
+
+        //int n_cadena;
+        //n_cadena = Convert.ToInt32(cadena2);
+        //Console.WriteLine(n_objeto);
+        //Console.WriteLine(n_cadena);
+
+        // string cadena = "555.10";
+
+        //float flota = float.Parse(cadena);
+        // int n_entero = int.Parse(cadena);
+        //char n_char = char.Parse(cadena);
+        //double n_doble = double.Parse(cadena);
+
+        //System.Console.WriteLine(flota);
+        //System.Console.WriteLine(n_entero);
+        //System.Console.WriteLine(n_char);
+        //System.Console.WriteLine(n_doble);
+
+        //float flota = 45.9F;
+        //string n_string = flota.ToString();
+        //System.Console.WriteLine(n_string);
+
+
+        //int primerNumero, segundoNumero, suma;
+        //string cadena = "";
+        //Console.Write("La operacion es: ");
+        //cadena = Console.ReadLine();
+
+        //Console.Write("Introduce el primer numero: ");
+        //int primerNumero = 10;
+        ////Console.Write("Introduce el segundo numero: ");
+        //int segundoNumero = 10;
+        //int suma = primerNumero + segundoNumero;
+
+        //Console.WriteLine("La suma es= " + suma);
+
+        ////https://learn.microsoft.com/es-es/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
+        //string variable = "9.223.372.036.854.775.807"; // numero entero positivo o negativo <=19 digitos 
+        //long dato_largo= long.Parse(variable);// numero entero positivo o negativo <=19 digitos 
+        //Console.WriteLine(dato_largo);
+
+        //Console.WriteLine("Ingrese un numero");
+        //int numero1= 25;
+        //Console.WriteLine("Ingrese  el segundo numero");
+
+        //string usuario = "progra";
+
+
+        //if (usuario == "programador")
+        //{
+        //    Console.WriteLine("Bienvendio");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("No existe usuario");
+        //}
+
+        //string condicion = "sol";
+
+        //if (condicion == "soleado")
+        //{
+        //    Console.WriteLine("Vamos a la playa");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Lo siento no podemos salir");
+        //}
+
+
+        //Console.WriteLine("Ingrese un numero");
+        //int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        //if (numero1 % 2 == 0)
+        //{
+        //    Console.WriteLine("El número es par");
+        //}
+        //else
+        //{
+
+        //    Console.WriteLine("El número es impar");
+
+        //}
+
+        //string contraseña = "1234";
+
+        //if (contraseña!="1234")
+        //{
+        //    Console.WriteLine("La contraseña es valida");
+        //}
+        //else
+        //{
+
+        //    Console.WriteLine("La contraseña no puede ser igual a 1234");
+
+        //}
+
+        //Console.WriteLine("Ingrese la nota final del alumno de 0 a 10");
+        //int nota_alumno = Convert.ToInt32(Console.ReadLine());
+
+        //if (nota_alumno>5)
+        //{
+        //    Console.WriteLine("El alumno ha aprovado con"+ nota_alumno);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("El alumno ha reprovado con"+ nota_alumno);
+        //}
+
+        //bool condicion1 = true;
+        //bool condicion2 = true;
+        //bool condicion3 = true;
+
+        //if (condicion1 && condicion2 && condicion3)
+        //{
+        //    Console.WriteLine("todo es verdadero o true");
+        //}
+        //else if (condicion1 || condicion2 || condicion3)
+        //{
+        //    Console.WriteLine("el varlor de alguno de los 3 es verdadero");
+
+        //}
+        //else
+        //{
+        //    Console.WriteLine("NO SE CUMPLE NINGUNA  CONDICION");
+        //}
+
+        //int lado1, lado2, lado3;
+        //lado1 =2;
+        //lado2=9;
+        //lado3=3;
+
+        //if(lado1 == lado2 && lado1== lado3 && lado2== lado3)
+        //{
+        //    Console.WriteLine("Equilatero");
+
+        //}
+        //else
+        //{
+        //    if ( lado1==lado2 || lado1==lado3 || lado2==lado3)
+        //    {
+        //        Console.WriteLine("Isosceles");
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Escaleno");
+        //    }
+        //}
+
+        //string numero;
+        //int opt = 5;
+
+        //switch (opt)
+        //{
+        //    case 1:
+        //        numero = "Uno";
+        //        break;
+        //    case 2:
+        //        numero = "Dos";
+        //        break;
+        //    case 3:
+        //        numero = "Tres";
+        //        break;
+        //    default:
+        //        numero = "Error o No se evaluo la condicion";
+        //        break;
+        //}
+
+        //Console.WriteLine("El numero es: "+numero);
+
+        int edad;
+        edad = 40;
+        switch (edad)
+        {
+            case >= 60:
+                Console.WriteLine("anciano");
+                break;
+            case >= 50:
+                Console.WriteLine("adulto mayor");
+                break;
+            case >= 30:
+                Console.WriteLine("adulto");
+                break;
+            case >= 20:
+                Console.WriteLine("joven");
+                break;
+            case >= 13:
+                Console.WriteLine("adolescente");
+                break;
+            case >= 10:
+                Console.WriteLine("puberto");
+                break;
+            case >= 4:
+                Console.WriteLine("infante");
+                break;
+            case >= 0:
+                Console.WriteLine("recien nacido");
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+
+
+        }
+
+
+
+
+
+    }
+}
